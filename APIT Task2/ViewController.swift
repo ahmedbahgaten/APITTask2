b73cee86147d10833ffabb2b5eff7e4a6dd8fcba
ahmedbahgaten <ahmedbahgatnabih@gmail.com>
2019-12-29 04:17:04 -0800

    intializing the task

---
+++ APIT Task2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  APIT Task2
+//
+//  Created by AHMED on 12/29/19.
+//  Copyright © 2019 AHMED. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '10.0'
+
+target 'APIT Task2' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for APIT Task
+pod 'Alamofire', '~> 5.0.0-rc.3'
+end
