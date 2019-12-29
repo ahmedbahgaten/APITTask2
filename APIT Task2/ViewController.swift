0d7f91c421d78be5252150f6e21b259459702d04
ahmedbahgaten <ahmedbahgatnabih@gmail.com>
2019-12-29 05:03:45 -0800

    setting the view of the tableview cell

--- APIT Task2/ViewController.swift
+++ APIT Task2/ViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    private var ReceivedData = [ServerResponse]()
     override func viewDidLoad() {
         super.viewDidLoad()
+        getDataFromAPI()
+        setTableViewDelegateAndDataSource()
     }
     
+    func getDataFromAPI () {
+        NetworkManager.DataFetching { (serverResponse, Error) in
+            guard let response = serverResponse else {return}
+            self.ReceivedData = response
+            
+        }
+    }
+    func setTableViewDelegateAndDataSource () {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
 
+extension ViewController: UITableViewDataSource,UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        ReceivedData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "myCell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier , for:indexPath ) as! APITTableViewCell
+        
+    }
+    
+    
 }
 
 
